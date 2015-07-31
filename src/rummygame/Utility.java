baaa776646bc625070544c6c01c6d3ef8aa5a96b
Manish Prajapati <manishengi25@gmail.com>
2015-07-31 15:39:19 +0530

    Difference function added

--- src/rummygame/Utility.java
+++ src/rummygame/Utility.java
@@ -16,6 +16,42 @@ public class Utility {
 		return sortedCardHand;
 
 	}
+	
+	public static int getdifference (String str1,String  str2)
+        {
+            String[] card1 = str1.split("\\|");
+            String[] card2 = str2.split("\\|");
+            
+            int [] intcards1 = new int[card1.length];
+            int [] intcards2 = new int[card2.length];
+            
+            for( int i=0;i<13;i++)
+            {
+                intcards1[i] = Integer.parseInt(card1[i]);
+                intcards2[i] = Integer.parseInt(card2[i]);
+            }
+            
+            Arrays.sort(intcards1);
+            Arrays.sort(intcards2);
+            
+           int a=0,b=0,count=0;
+           while(a<13&&b<13)
+           {
+               if(intcards1[a]>intcards2[b])
+                   b++;
+               else if(intcards1[a]<intcards2[b])
+                   a++;
+               else
+               {
+                   a++;
+                   b++;
+                   count++;
+               }
+               
+           }
+           
+            return 13-count;
+        }
 
 	public static boolean sanityCheck(String cardHand) {
 		boolean sanityCheck = true;
